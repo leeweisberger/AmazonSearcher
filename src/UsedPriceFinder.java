2b708b0f19dcacc5db6e7f200ed5118fe42e8aa5
lee.weisberger <lee.weisberger@duke.edu>
2015-05-05 19:04:34 -0400

    hopefully only sending once a day. Added connection to dropbox file

--- src/UsedPriceFinder.java
+++ src/UsedPriceFinder.java
@@ -2,36 +2,38 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TimeZone;
+
+import com.dropbox.core.DbxException;
 
 
 public class UsedPriceFinder {
 	private static final String TIME_ZONE = "America/New_York";
 	private static final int MS_IN_DAY = 86400000;
-	private static final String LAST_DAY_TXT = "last_day.txt";
-	public static final String FILE_NAME = "Amazon_Form.xlsm.csv";
-	public static final int BEGIN_HOUR = 6;
-	public static final int END_HOUR = 23;
+	public static final String LAST_DAY_TXT = "last_day.txt";
+	public static final String LAST_DAY_TXT_LOCATION = "temp\\"+LAST_DAY_TXT;
+	public static final String CSV_FILE_NAME = "Amazon_Form.xlsm.csv";
+	private static final int BEGIN_HOUR = 6;
+	private static final int END_HOUR = 23;
+	private static Set<String> toWriteToFile = new HashSet<String>();
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, DbxException, IOException {
 		
 		if(isOutOfTimeRange(BEGIN_HOUR, END_HOUR))
 			return;
+		DropBoxWriter.downloadAndSaveToFile();
 		System.out.println(System.currentTimeMillis());
-		Map<String, Map<String,Double>> minPriceMap = FileParser.readFileToMap(FILE_NAME);
+		Map<String, Map<String,Double>> minPriceMap = FileParser.readFileToMap(CSV_FILE_NAME);
 		Map<String, Map<String,Double>> underMap = new HashMap<String, Map<String,Double>>();
 		Map<String, String> urlMap = new HashMap<String,String>();
 		for(String asin : minPriceMap.keySet()){
@@ -44,6 +46,9 @@ public class UsedPriceFinder {
 			if(map.size()>0 && !wasSentLastDay(asin))
 				underMap.put(asin, map);
 		}
+		if(toWriteToFile.size()>0)
+			DropBoxWriter.updateFile(toWriteToFile);
+
 		if(underMap.size()>0)
 			Emailer.sendEmail(underMap, minPriceMap, urlMap);
 
@@ -67,7 +72,7 @@ public class UsedPriceFinder {
 	private static boolean wasSentLastDay(String asin) {
 		FileInputStream fstream=null;
 		try {
-			fstream = new FileInputStream(LAST_DAY_TXT);
+			fstream = new FileInputStream(LAST_DAY_TXT_LOCATION);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -88,26 +93,19 @@ public class UsedPriceFinder {
 					isInFile=true;
 				}
 				if(System.currentTimeMillis()-Long.valueOf(asinTimeList[1])<MS_IN_DAY){
-					linesToKeep.add(line);
+					toWriteToFile.add(line + "\n");
 				}
 			}
 			br.close();
-			PrintWriter writer = new PrintWriter(LAST_DAY_TXT);
-			writer.print("");
-			writer.close();
-			FileWriter fw = new FileWriter(LAST_DAY_TXT,true);
-			for(String str : linesToKeep){
-				fw.write(str+"\n");
-			}
-			if(!isInFile || isExpired)
-				fw.write(asin + "," + System.currentTimeMillis()+"\n");
-			fw.close();
 
+			if(!isInFile || isExpired)
+				toWriteToFile.add(asin + "," + System.currentTimeMillis()+"\n");
 
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 		}
+		 
 		return isInFile && !isExpired;
 
 	}
